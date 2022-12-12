21af647114f7e16116b044ac1a72bd137431a0cf
Kuldeep Singh <kuldeepretro8437@gmail.com>
2022-12-13 00:04:35 +0530

    update-2

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import db from "./config/database.js";
 import productRoutes from "./routes/Index.js";
 import cors from "cors";
- 
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
  
 try {
@@ -11,9 +12,12 @@ try {
 } catch (error) {
     console.error('Connection error:', error);
 }
+app.get('/', (req, res) => {
+    res.send('Hello to Yoga-Hub API');
+  });
  
 app.use(cors());
 app.use(express.json());
 app.use('/users', productRoutes);
  
-app.listen(process.env.PORT || 5000, () => console.log('Server running!!')); 
+app.listen(5000, () => console.log('Server running at port 5000'));
